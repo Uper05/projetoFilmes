04501bee6ad0b0323945ca97e117dd397d06d9d9
Uper05 <jvfdessani@gmail.com>
2023-11-01 09:38:57 -0300

    Foda

---
+++ front/js/scripts.js
@@ -0,0 +1,18 @@
+
+const protocolo = "http://"
+const baseURL = "localhost:3000"
+const filmesEndPoint = "/filmes"
+
+async function obterFilmes(){
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for(let filme of filmes){
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require ('express')
+const cors = require('cors')
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 let filmes = [
     {
     titulo: "Forrest Gump - O Contador de Histórias",
